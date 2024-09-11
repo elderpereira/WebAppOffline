8ada1f9134d3cf42ba2e26741d70bb9b4d8aef24
elderpereira <minieldir@gmail.com>
2024-09-11 11:44:44 -0300

    V0.1

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,11 @@ const urlsToCache = [
   '/styles.css',
   '/script.js',
   '/icon-192x192.png',
-  '/icon-512x512.png'
+  '/icon-512x512.png',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
+  'https://code.jquery.com/jquery-3.5.1.slim.min.js',
+  'https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js',
+  'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js'
 ];
 
 self.addEventListener('install', event => {
