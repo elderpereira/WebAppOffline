0b590b69a8cae3058a398366fb497e0c4fc2b626
elderpereira <minieldir@gmail.com>
2024-09-11 17:20:17 -0300

    v1.2.7

--- service-worker.js
+++ service-worker.js
@@ -28,7 +28,7 @@ self.addEventListener('fetch', event => {
         if (response) {
           return response;
         }
-        return fetch(event.request);
+        return fetch(event.request).catch(() => caches.match('/index.html'));
       })
   );
 });
