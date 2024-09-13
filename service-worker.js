2063f832c338cff8a64cb0e2cecf358aee5e8279
elderpereira <minieldir@gmail.com>
2024-09-13 11:04:20 -0300

    v1.3.77

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,63 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('app-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/manifest.json',
-        '/icon-192x192.png',
-        '/icon-512x512.png',
-        '/service-worker.js',
-        'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'
-      ]);
-    })
-  );
+const cacheName = 'webappoffline-cache';
+
+self.addEventListener('install', function (event) {
+    event.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll([
+                './',
+                './index.html',
+                './manifest.json',
+                './icon-192x192.png',
+                './icon-512x512.png',
+                './service-worker.js',
+                'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css'
+            ]);
+        })
+    );
+    return self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('activate', function (event) {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName !== 'webappoffline-cache';
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+    return self.clients.claim();
 });
+
+self.addEventListener('fetch', function (event) {
+    const req = event.request;
+    const url = new URL(req.url);
+
+    if (url.origin === location.origin) {
+        event.respondWith(cacheFirst(req));
+    } else {
+        event.respondWith(networkAndCache(req));
+    }
+});
+
+async function cacheFirst(req) {
+    const cache = await caches.open(cacheName);
+    const cached = await cache.match(req);
+
+    return cached || fetch(req);
+}
+
+async function networkAndCache(req) {
+    const cache = await caches.open(cacheName);
+    try {
+        const fresh = await fetch(req);
+        await cache.put(req, fresh.clone());
+        return fresh;
+    } catch (e) {
+        const cached = await cache.match(req);
+        return cached;
+    }
+}
